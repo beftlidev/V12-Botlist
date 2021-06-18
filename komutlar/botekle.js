550163dff2b1d17e70f920ee634fd99e3acebd85
iUgur <77486662+iUgur@users.noreply.github.com>
2021-06-18 18:40:29 +0300

    Update botekle.js

--- komutlar/botekle.js
+++ komutlar/botekle.js
@@ -1,6 +1,5 @@
 const discord = require("discord.js");
 const db = require("croxydb");
-const { MessageButton } = require("discord-buttons") 
 
 exports.run = async (client, message, args) => {
  let blue = new discord.MessageEmbed().setColor("BLUE");
@@ -14,21 +13,11 @@ let sira = db.fetch(`serverData.${message.guild.id}.waitSize`) || 0;
   let botprefix = args[1];
   if(!botid) return message.inlineReply('<:codesty_cross:844468546930606100> Lütfen bir bot ID gir!')
   if(!botprefix) return message.inlineReply('<:codesty_cross:844468546930606100> Lütfen bir bot Prefix gir!')
-
-let perm = new MessageButton()
-    .setLabel("0 Perm")
-    .setURL(`https://discord.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=0`)
-    .setStyle("url"); 
-
-let perm2 = new MessageButton()
-    .setLabel("8 Perm")
-    .setURL(`https://discord.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=8`)
-    .setStyle("url"); 
-
   let embed = new discord.MessageEmbed()
   .setAuthor(message.author.username, message.author.displayAvatarURL({dynamic:true}))
-  .addField("Bot ID", botid, true)
-  .addField("Bot Prefix", botprefix, true)
+  .addField("<:codesty_join:844468549417697350> Bot ID", botid, true)
+  .addField("<:codesty_join:844468549417697350> Bot Prefix", botprefix, true)
+  .addField("<:codesty_join:844468549417697350> Bot Davet", "<:codesty_join:844468549417697350> Davet: [0 Perm](https://discord.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=0) **|** [8 Perm](https://discord.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=8)") 
   .setColor("BLUE")
   .setFooter("Space Giveaway")
   let botvaryok = db.fetch(`bot.id.${botid}`)
@@ -37,10 +26,10 @@ let perm2 = new MessageButton()
       if(botvaryok) {
         message.inlineReply('<:codesty_cross:844468546930606100> Bu bot zaten sistemde var!')
       } else {
-      if(message.channel.id !== basvuru) return message.reply('<:codesty_cross:844468546930606100> Lütfen bunu <#'+basvuru+'> kanalında dene!')
+      if(message.channel.id !== basvuru) return message.reply('<:codesty_cross:844468546930606100> Lütfen <#${başvuru}> kanalında bot ekle!')
       if(client.channels.cache.get(basvuru).send(embed));
       if(client.channels.cache.get(botlog).send(blue.setDescription(`<:codesty_check:844468545877442560> Bir bot eklendi!
-<:codesty_join:844468549417697350> Bot: ${botid} **|** <@${botid}> **|** <@\`${botid}\`>
+<:codesty_join:844468549417697350> Bot: ${botid} **|** <@${botid}>
 <:codesty_join:844468549417697350> Sahip: ${message.author}
 <:codesty_join:844468549417697350> Sıra: ${sira}
 <:codesty_join:844468549417697350> Davet: [0 Perm](https://discord.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=0) **|** [8 Perm](https://discord.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=8)`)));
