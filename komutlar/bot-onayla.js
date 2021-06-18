f1586d8c085ec901a9515bed74fbbb291dd62096
iUgur <77486662+iUgur@users.noreply.github.com>
2021-06-18 18:24:32 +0300

    Update bot-onayla.js

--- komutlar/bot-onayla.js
+++ komutlar/bot-onayla.js
@@ -6,16 +6,16 @@ exports.run = async (client, message, args) => {
   if(!onayred) return message.inlineReply('<:codesty_cross:844468546930606100> Onay-Red log kanalı ayarlı değil!")
   let yetkili = db.fetch(`bot.yetkili.rol.${message.guild.id}`);
   if(!yetkili) return message.inlineReply('<:codesty_cross:844468546930606100> Bot Yetkilisi rolü ayarlı değil!")
-  if(!message.member.roles.cache.has(yetkili)) return message.reply('<:codesty_cross:844468546930606100> Bot yetkilisi değilsin!")
+  if(!message.member.roles.cache.has(yetkili)) return message.reply('<:codesty_cross:844468546930606100> Bot yetkilisi değilsin!')
   let botid = args[0];
-  if(!botid) return message.inlineReply('<:codesty_cross:844468546930606100> Lütfen bir bot ID gir!")
+  if(!botid) return message.inlineReply('<:codesty_cross:844468546930606100> Lütfen bir bot ID gir!')
   let botvaryok = db.fetch(`bot.id.${botid}`)
   if(botid) {
     if(!botvaryok) {
       return message.inlineReply('<:codesty_cross:844468546930606100> Sistemde böyle bir bot yok!')
     } else {
-    if(client.channels.cache.get(onayred).send('<:codesty_cross:844468546930606100> <@"+botid+"> adlı bot <@"+message.author.id+"> tarafından onaylandı!"));
-    message.inlineReply('<:codesty_check:844468545877442560> <@"+botid+"> adlı bot onaylandı!")
+    if(client.channels.cache.get(onayred).send(`<:codesty_cross:844468546930606100> <@"+botid+"> adlı bot <@"+message.author.id+"> tarafından onaylandı!`));
+    message.inlineReply(`<:codesty_check:844468545877442560> <@"+botid+"> adlı bot onaylandı!`)
     db.delete(`bot.id.${botid}`)
     db.subtract(`serverData.${message.guild.id}.waitSize`, 1)
     }
